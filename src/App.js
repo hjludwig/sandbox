199d415480af46e63356ed4e26ed8c5730d74e4e
Jeremy Ludwig <65617794+hjludwig@users.noreply.github.com>
2021-03-09 11:14:37 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.elements);
+    setName(e.target.elements.name.value);
+  };
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Your name: </label>
+        <input id="name" type="text" />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <h1>Hello {name}, your sauce is ready.</h1>
+      </div>
+    </div>
+  );
+}
